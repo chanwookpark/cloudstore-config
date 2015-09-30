2620a450d47ebef5b6215f052c9fc8eba06d1f0a
chanwook <chanwook.god@gmail.com>
2015-10-01 04:14:26 +0900

    starting with aws. add yml and divide exit file.

--- src/main/java/io/clouldstore/config/ConfigApplication.java
+++ src/main/java/io/clouldstore/config/ConfigApplication.java
@@ -2,14 +2,31 @@ package io.clouldstore.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author chanwook
  */
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigApplication {
+@RestController
+public class ConfigApplication extends SpringBootServletInitializer {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello!!";
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConfigApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
